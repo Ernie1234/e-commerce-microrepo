a291c26ea035ac5743efdcbaa1096de2b8d16d19
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-06-11 02:00:16 +0100

    feat: disable swagger ui

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import swagerUI from 'swagger-ui-express';
-import swaggerDocument from './utils/docs/swagger-output.json';
+// import swagerUI from 'swagger-ui-express';
+// import swaggerDocument from './utils/docs/swagger-output.json';
 
 import router from './routes/auth-router';
 import { errorMiddleware } from '@packages/error-handler/error-middleware';
@@ -24,11 +24,11 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api-docs', swagerUI.serve, swagerUI.setup(swaggerDocument));
-app.get('/docs-json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(swaggerDocument);
-});
+// app.use('/api-docs', swagerUI.serve, swagerUI.setup(swaggerDocument));
+// app.get('/docs-json', (req, res) => {
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(swaggerDocument);
+// });
 
 app.get('/', (req, res) => {
   res.send({ message: 'Welcome auth API' });
@@ -40,7 +40,7 @@ app.use(errorMiddleware);
 
 const server = app.listen(port, () => {
   console.log(`👱 Auth service is running at http://localhost:${port}/auth`);
-  console.log(`📝 Swagger Docs available at http://localhost:${port}/docs`);
+  // console.log(`📝 Swagger Docs available at http://localhost:${port}/docs`);
 });
 
 server.on('error', (err) => {
